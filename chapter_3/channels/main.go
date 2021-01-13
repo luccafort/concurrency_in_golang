ff25e6e20848c2cbf7a15dc82f23265570ce88da
luccafort <konishi.tatsuro@moneyforward.co.jp>
2021-01-14 00:36:09 +0900

    adding to invalid channels usage.

--- chapter_3/channels/main.go
+++ chapter_3/channels/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // p.66
 // func main() {
@@ -24,3 +22,16 @@ func main() {
 
 	fmt.Println(<-stringStream)
 }
+
+// invalid channels
+// func main() {
+// 	// $ go run main.go
+// 	// # command-line-arguments
+// 	// ./main.go:29:2: invalid operation: <-writeStream (receive from send-only type chan<- interface {})
+// 	// ./main.go:30:13: invalid operation: readStream <- struct {} literal (send to receive-only type <-chan interface {})
+// 	writeStream := make(chan<- interface{})
+// 	readStream := make(<-chan interface{})
+
+// 	<-writeStream
+// 	readStream <- struct{}{}
+// }
