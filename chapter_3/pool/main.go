835c89ac5311f9f28b6ada961ac2cdf4eca4d103
luccafort <konishi.tatsuro@moneyforward.co.jp>
2021-01-13 22:00:34 +0900

    sync.Poolを使ったメモリアロケーションの制御

--- chapter_3/pool/main.go
+++ chapter_3/pool/main.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// p.60
 func main() {
 	myPool := &sync.Pool{
 		New: func() interface{} {
@@ -18,3 +19,40 @@ func main() {
 	myPool.Put(instance)
 	myPool.Get()
 }
+
+//p.61
+// func main() {
+// 	var numCalcsCreated int
+// 	calcPool := &sync.Pool{
+// 		New: func() interface{} {
+// 			numCalcsCreated++
+// 			mem := make([]byte, 1024)
+// 			return &mem
+// 		},
+// 	}
+
+// 	// プールに4kb確保する
+// 	calcPool.Put(calcPool.New())
+// 	calcPool.Put(calcPool.New())
+// 	calcPool.Put(calcPool.New())
+// 	calcPool.Put(calcPool.New())
+
+// 	const numWorkers = 1024 * 1024
+// 	var wg sync.WaitGroup
+// 	wg.Add(numWorkers)
+
+// 	for i := numWorkers; i > 0; i-- {
+// 		go func() {
+// 			defer wg.Done()
+
+// 			mem := calcPool.Get().(*[]byte)
+// 			defer calcPool.Put(mem)
+
+// 			// なにか面白いことを行う
+// 			// ただしメモリに対して素早い処理が行われること
+// 		}()
+// 	}
+
+// 	wg.Wait()
+// 	fmt.Printf("%d calculators were created.", numCalcsCreated)
+// }
